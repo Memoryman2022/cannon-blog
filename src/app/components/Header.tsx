35907bf240cce3916553dd1149094a03df45301f
Robert Cannon <rlacannon@gmail.com>
2026-01-21 13:30:00 +0100

    connected to cluster/header styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="p-4 bg-gray-100 flex justify-between items-center">
+    <nav className="p-4 bg-red-800 flex justify-between items-center">
       <h1 className="font-bold text-xl">Cannon Blog</h1>
       <div className="space-x-4">
-        <Link href="/" className="text-blue-600 hover:underline">
+        <Link href="/" className="text-white hover:underline">
           Home
         </Link>
-        <Link href="/admin/add-post" className="text-blue-600 hover:underline">
+        <Link href="/admin/add-post" className="text-white hover:underline">
           Add Post (Admin)
         </Link>
       </div>
