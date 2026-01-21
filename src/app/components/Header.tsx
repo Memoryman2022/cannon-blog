7878f08c23bab5de042e4b913a9c4b92c6556fe4
Robert Cannon <rlacannon@gmail.com>
2026-01-21 12:12:46 +0100

    Initial Next.js + TypeScript + Tailwind setup

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="border-b p-4">
+      <div className="mx-auto max-w-5xl">
+        <h1 className="text-xl font-bold text-red-600">
+          Cannon Blog
+        </h1>
+      </div>
+    </header>
+  );
+}
